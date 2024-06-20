e4eea92bb3328e565d3c82ce7c915fa5e2d7b0b2
AKASH YADAV <yakashadav26@gmail.com>
2024-06-20 16:41:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -122,6 +122,7 @@ async def send_email(email_data: EmailData):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
 
 
